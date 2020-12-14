b46ca2851c9a877d488407b23a6a95d784e5afcb
Programs322 <76015239+Programs322@users.noreply.github.com>
2020-12-14 17:04:57 -0500

    Create vidlii.js

---
+++ vidlii.js
@@ -0,0 +1,9 @@
+function videoTutorial() {
+  document.getElementById("ae2x0c").style.display = "none";
+  document.getElementById("nltx34a").style.display = "block";
+}
+
+function tutorialClose() {
+  document.getElementById("ae2x0c").style.display = "block";
+  document.getElementById("nltx34a").style.display = "none";
+}
